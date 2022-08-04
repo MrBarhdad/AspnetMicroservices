c243399fecbe8657ad14a181ed3b30b439335323
mohamed barhdad <mohamed.barhdad@gmail.com>
2022-08-04 15:02:08 +0100

    Basket MicroServices Developed

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string username)
+        {
+            this.UserName = username;
+        }
+
+
+
+        public decimal TotalPrice { get
+            {
+                decimal tolatprice = 0;
+                foreach (var item in Items)
+                {
+                    tolatprice += item.Price * item.Quantity;
+                }
+                return tolatprice;
+            } 
+        }
+    }
+}
